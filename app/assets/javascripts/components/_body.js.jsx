ccf02d2339447924cdc822ef443169f0464d57a8
Beth Sebian <esebian@gmail.com>
2016-04-14 20:01:36 -0600

    7: Add a skill

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,8 +1,22 @@
 var Body = React.createClass({
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  handleSubmit(skill) {
+    var newState = this.state.skills.concat(skill);
+    this.setState({ skills: newState })
+  },
+
   render() {
     return (
       <div>
-        <AllSkills />
+        <NewSkill handleSubmit={this.handleSubmit} />
+        <AllSkills skills={this.state.skills} />
       </div>
     )
   }
